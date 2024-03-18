c78a3af3a8d2fe5c7a3efa256360eb5fbdf74a62
ignasimgol <ignasimgol@gmail.com>
2024-03-18 10:27:07 +0100

    portfolio v1

---
+++ js/projects.js
@@ -0,0 +1,65 @@
+const blogPosts = [
+    {
+        title: 'THE MAGIC PORTAL',
+        time: 'three.js · JS',
+        image: '/img/portal1.jpg', 
+        link: 'https://threedportal.vercel.app/'
+    },
+    {
+        title: 'PHOTOGRAPHER PORTFOLIO',
+        time: 'HTML5 · CSS3 · JS',
+        image: '/img/bymariadelrio.jpg', 
+        link: 'https://bymariadelrio-portfolio.vercel.app/' 
+    },
+    {
+        title: 'THIS PORTFOLIO',
+        time: 'HTML5 · CSS3 · JS',
+        image: '/img/iggy.jpg', 
+        link: 'street.html' 
+    }
+]
+
+const createBlogposts = () => {
+    blogPosts.forEach(post => {
+        let blogPostSection = document.createElement('div');
+        blogPostSection.classList.add('blog__post');
+
+        let postDiv = document.createElement('div');
+        postDiv.classList.add('post');
+
+        let imageContainer = document.createElement('div');
+        imageContainer.classList.add('post__image__container');
+
+        let imageLink = document.createElement('a');
+        imageLink.href = post.link; // Establece la URL de la página HTML
+
+        let image = document.createElement('img');
+        image.classList.add('blog__post__img');
+        image.src = post.image;
+
+        imageLink.appendChild(image); // Coloca la imagen dentro del enlace
+
+        let postDetails = document.createElement('div');
+        postDetails.classList.add('post__details');
+
+        let postTitle = document.createElement('p');
+        postTitle.innerText = post.title;
+
+        let postTime = document.createElement('p');
+        postTime.innerText = post.time;
+
+        postDetails.append(postTitle, postTime);
+
+        imageContainer.appendChild(imageLink); // Coloca el enlace en lugar de la imagen directamente
+        postDiv.append(imageContainer, postDetails);
+        blogPostSection.appendChild(postDiv);
+
+        document.getElementById('blog').appendChild(blogPostSection);
+    })
+}
+
+
+
+export {
+    createBlogposts
+}
